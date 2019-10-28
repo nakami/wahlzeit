bff8775a1707ebe8c7474f660a079fbfc1040c7d
nakami <max.lattka@studium.fau.de>
2019-10-28 02:26:39 +0100

    Add getter/setter-methods to Coordinate class

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -11,6 +11,30 @@ public class Coordinate{
         this.z = z;
     }
 
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public double getZ(){
+        return this.z;
+    }
+
+    public void setX(double x){
+        this.x = x;
+    }
+
+    public void setY(double y){
+        this.y = y;
+    }
+
+    public void setZ(double z){
+        this.z = z;
+    }
+
     public double getDistance(Coordinate c){
         double sum = Math.pow(this.x - c.x, 2)
                     + Math.pow(this.y - c.y, 2)
